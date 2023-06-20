091030090b0eba8f4317d85fc337d0527bec9455
suraratkhanthong <khanthongsurarat@gmail.com>
2023-06-21 02:57:59 +0700

    edit1

--- public/src/index.js
+++ public/src/index.js
@@ -11,20 +11,27 @@ customElements.define("header-app", header)
 
 window.onscroll = function() {myFunction()};
 
+let BtnMenu = document.querySelector(".menu");
+let NavList = document.querySelector(".nav__list");
+let coutCliackMenu = 0;
+
 function myFunction() {
 	let scrollTop = document.documentElement.scrollTop;
 	let HeaderApp = document.querySelector(".Header-app");
     if(scrollTop >= 110){
-		HeaderApp.style.visibility = "visible";
+		// HeaderApp.style.visibility = "visible";
 	}
 	if( scrollTop < 110){
-		HeaderApp.style.visibility = "hidden";
+		// HeaderApp.style.visibility = "hidden";
+		
+		// BtnMenu.classList.remove("opened")
+		// NavList.classList.remove("nav__menu-lists")
+		// coutCliackMenu=0;
 	}
 }
-let coutCliackMenu = 0;
+
 function clickMenu(){
-	let BtnMenu = document.querySelector(".menu");
-	let NavList = document.querySelector(".nav__list");
+	//alert(coutCliackMenu)
 	if (coutCliackMenu === 0) {
       BtnMenu.classList.add("opened")
       NavList.classList.add("nav__menu-lists")
